c5886886e6a75225c6aac543665a1d7792da08fa
Joseph Davis <joethebartenderpdx@gmail.com>
2021-08-02 17:42:54 -0700

    slight style change

--- src/components/quote/Quote.jsx
+++ src/components/quote/Quote.jsx
@@ -4,6 +4,7 @@ import style from './Quote.module.css';
 export function Quote({ quote }) {
   return (
     <div className={style.quoteDisplay}>
+      <h1>Random Rick and Morty Character</h1>
       <h2>{JSON.stringify(quote)}</h2>
     </div>
   );
